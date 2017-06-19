6ef583c24471e997dc39b65d0b55ea774d7a94a1
Cottonwood Devcamp <cottonwood@devcamp.com>
2017-06-19 15:53:42 +0000

    Integrated skills via model generator

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,7 @@
 class PagesController < ApplicationController
   def home
-<<<<<<< HEAD
     @posts = Blog.all
     @skills =skill.all
-=======
->>>>>>> 5bd0da368a59921afacf19123995b29a636b8713
   end
 
   def about
