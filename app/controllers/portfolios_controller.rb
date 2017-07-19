0b8f399ea2ecf0b4d3a5b6fe92bfc061cd4dca74
Cottonwood Devcamp <cottonwood@devcamp.com>
2017-07-19 15:38:57 +0000

    Updated portfolio to include attr for position

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -6,7 +6,7 @@ class PortfoliosController < ApplicationController
   site_admin: :all
   
     def index
-       @portfolio_items = Portfolio.all
+       @portfolio_items = Portfolio.by_position
     end
     
     def angular
